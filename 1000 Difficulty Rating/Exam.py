1c64151294f091a27dd744d6e1199b1677444971
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-01-13 15:29:58 +0530

    Update Exam.py

--- 1000 Difficulty Rating/Exam.py
+++ 1000 Difficulty Rating/Exam.py
@@ -1,1 +1,11 @@
+#coock your dish here
 
+t = int(input())
+for _ in range(t):
+    x,y,z = map(int,input().split())
+    tot = x * y 
+    per = (z/tot)*100
+    if per > 50:
+        print("yes")
+    else:
+        print("no")
