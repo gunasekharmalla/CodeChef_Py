099ca16d56588827c8e47730b2491d03be43f2c5
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-01-28 06:54:07 +0530

    Update Mahasena.py

--- 1000 Difficulty Rating/Mahasena.py
+++ 1000 Difficulty Rating/Mahasena.py
@@ -1,1 +1,15 @@
+# cook your dish here
 
+n = int(input())
+lst = list(map(int,input().split()))
+even = 0
+odd = 0
+for i in lst:
+    if i % 2 == 0:
+        even = even + 1
+    else:
+        odd = odd + 1
+if even > odd:
+   print("READY FOR BATTLE")
+else:
+    print("NOT READY")
