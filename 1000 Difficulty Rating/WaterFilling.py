2b046d550b6082941ea012e48969b0921c4dcd0b
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-26 09:41:37 +0530

    Update WaterFilling.py

--- 1000 Difficulty Rating/WaterFilling.py
+++ 1000 Difficulty Rating/WaterFilling.py
@@ -1,1 +1,14 @@
 
+# cook your dish here
+
+for _ in range(int(input())):
+    x,y,z = map(int,input().split())
+    if x==0 and y==0 and z==0:
+        print("Water filling time")
+    elif x==1 and y==1 and z==1:
+        print("Not now")
+    elif ((x==0 and y==0 and z==1) or (x==1 and y==0 and z==0) or (x==0 and y==1 and z==0)):
+        print("Water filling time")
+    elif ((x==1 and y==1 and z==0)or(x==0 and y==1 and z==1)or(x==1 and y==0 and z==1)):
+        print("Not now")
+    
