0934fa2f07139c7fd5d6e218e64748130ebf8244
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-26 09:43:17 +0530

    Update SaleSeason.py

--- 1000 Difficulty Rating/SaleSeason.py
+++ 1000 Difficulty Rating/SaleSeason.py
@@ -1,1 +1,13 @@
 
+# cook your dish here
+
+for _ in range(int(input())):
+    x = int(input())
+    if x <= 100:
+        print(x)
+    elif 100 < x <= 1000:
+        print(x-25)
+    elif 1000 < x <= 5000:
+        print(x-100)
+    elif x > 5000:
+        print(x-500)
